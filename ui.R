91f91d675ff921a8c9ada3ef387e3dd4e70ab051
Ari Lamstein <arilamstein@gmail.com>
2015-08-23 10:43:20 -0700

    initial prod checkin

--- ui.R
+++ ui.R
@@ -4,11 +4,15 @@ library(choroplethr)
 data(df_county_demographics)
 options = colnames(df_county_demographics)[-1]
 
+long_text = paste("Data comes from the 2010 and 2013 American Community Survey (ACS).<br/>",
+                  "Want to make maps like this? Take my free course <a href='http://www.arilamstein.com/free-course'>Learn to Map Census Data in R</a>.")
+
 shinyUI(fluidPage(
 
   titlePanel("Mapping US Demographic Changes: 2010 to 2013."),
   div(HTML("By <a href='http://www.arilamstein.com'>Ari Lamstein</a>.")),
-  div(HTML("Data comes from the 2010 and 2013 <a href='https://en.wikipedia.org/wiki/American_Community_Survey'>American Community Survey (ACS).</a>")),
+  
+  div(HTML(long_text)),
 
   sidebarLayout(
     sidebarPanel(
@@ -16,7 +20,7 @@ shinyUI(fluidPage(
     ),
 
     mainPanel(
-      plotOutput("stateMap")
+      imageOutput("stateMap", width="640px", height="480px")
     )
   )
 ))
